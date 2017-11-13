4b743396e26c366920fdde68eb76a3eb3c9df016
Andreas Wawrzinek <info@iscape.de>
2017-11-13 09:00:11 +0100

    Update Reusable

--- src/app/button/button.module.ts
+++ src/app/button/button.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseButtonComponent } from './base-button/base-button.component';
-import { MyButtonComponent } from './my-button/my-button.component';
 
 @NgModule({
     imports: [
         CommonModule
     ],
-    declarations: [BaseButtonComponent, MyButtonComponent],
-    exports: [BaseButtonComponent, MyButtonComponent],
+    declarations: [BaseButtonComponent],
+    exports: [BaseButtonComponent],
 })
 export class ButtonModule {
 }
